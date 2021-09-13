31297bb7983892961608524c212940843aedaef1
RagulKanna <ragulragi619@gmail.com>
2021-09-13 10:37:10 +0530

    Added:Cards are shuffled using arraylist

--- src/main/java/Workshop/DeckOfCards/DeckOfcards.java
+++ src/main/java/Workshop/DeckOfCards/DeckOfcards.java
@@ -6,5 +6,6 @@ public class DeckOfcards extends InitializeCards {
         distribute();
         createplayers();
         distributecard();
+        shufflecards();
     }
 }
--- src/main/java/Workshop/DeckOfCards/InitializeCards.java
+++ src/main/java/Workshop/DeckOfCards/InitializeCards.java
@@ -1,23 +1,27 @@
 package Workshop.DeckOfCards;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
 public class InitializeCards {
 
         public static String[] suit={"Clubs","Diamonds", "Hearts", "Spades"};
-        public static String[] rank={"2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace"};
+        public static String[] rank={"-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-10","-Jack", "-Queen", "-King", "-Ace"};
         public static int CARDS=9;
         public static int PLAYERS;
-        public static int[] Players=new int[PLAYERS];
-        public static String[][] cardposition=new String[4][13];
+        public static ArrayList cards=new ArrayList();
+        public static ArrayList numbers=new ArrayList();
         public static Scanner sc =new Scanner(System.in);
 
     public static void distribute() {
+
         for (int i = 0; i < suit.length; i++) {
             for (int j = 0; j < rank.length; j++) {
                 String s1 = suit[i];
                 String s2 = rank[j];
-                cardposition[i][j] = s1.concat(s2);
+                cards.add(s1.concat(s2));
 
             }
         }
@@ -25,12 +29,8 @@ public class InitializeCards {
     }
         public static void print(){
             System.out.println("\nThe list of cards:");
-        for(int i=0;i< suit.length;i++) {
-            System.out.println("\n");
-            for (int j = 0; j < rank.length; j++) {
-
-                System.out.println(cardposition[i][j]);
-            }
+        for(int i=0;i<52;i++){
+            System.out.println(cards.get(i));
         }
     }
 
@@ -38,6 +38,7 @@ public class InitializeCards {
         System.out.println("\nEnter how many player you need:");
         PLAYERS = sc.nextInt();
         System.out.println("Created " + PLAYERS + " players");
+
     }
 
     public static void distributecard() {
@@ -46,6 +47,14 @@ public class InitializeCards {
 
             }
         }
+    }
+
+    public static void shufflecards(){
+        for(int i=0;i<52;i++){
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        System.out.println(numbers);
     }
 }
 
