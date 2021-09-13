d0de6bb936ae6386a87482c893b37ee0aac1ab8d
RagulKanna <ragulragi619@gmail.com>
2021-09-13 09:00:12 +0530

    Added:Welcome to game

---
+++ src/main/java/Workshop/DeckOfCards/InitializeCards.java
@@ -0,0 +1,8 @@
+package Workshop.DeckOfCards;
+
+public class InitializeCards {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome to deckofcards Workshop");
+    }
+
+}
